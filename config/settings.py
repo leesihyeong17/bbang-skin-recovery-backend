a3b56cc11846beb11508035714aa032afb346402
leesihyeong17 <dltlgud7901@gmail.com>
2026-08-19 01:57:27 +0900

    fix:settings.py allowed host []삭제

--- config/settings.py
+++ config/settings.py
@@ -40,8 +40,8 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [get_secret("ALLOWED_HOSTS")]
-CSRF_TRUSTED_ORIGINS = [get_secret("CSRF_TRUSTED_ORIGINS")]
+ALLOWED_HOSTS = get_secret("ALLOWED_HOSTS")
+CSRF_TRUSTED_ORIGINS = get_secret("CSRF_TRUSTED_ORIGINS")
 
 
 # Application definition
