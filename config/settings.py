27bed539bafe08595a1f3bbfd7b3e99a29fb96b0
leesihyeong17 <dltlgud7901@gmail.com>
2026-08-19 20:43:43 +0900

    fix:프론트 cors 주소 수정

--- config/settings.py
+++ config/settings.py
@@ -209,7 +209,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5173",
-    "https://www.naranhi.duckdns.org/"
+    "https://www.naranhi.duckdns.org",
+    "https://naranhi.duckdns.org"
 ]
 
 # ============================================================
